ea1a6463ebc9282fe772f757f0b8fb3390737a81
c0nt1 <silent1189@163.com>
2025-02-20 03:15:33 +0800

    编写好readme.md，调整index.ts，确保合并到main没有异常

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { Plugin, showMessage } from "siyuan";
 import * as api from "./api";
 import imageCompression from "browser-image-compression";
-import { resolveSoa } from "dns";
 
 export default class PluginSample extends Plugin {
   private Hpath: string;
@@ -10,11 +9,9 @@ export default class PluginSample extends Plugin {
   private compressSuffix: string = "webp";
 
   async onload(): Promise<void> {
-    console.log("插件已加载");
   }
 
   async onunload(): Promise<void> {
-    console.log("插件已卸载");
   }
 
   async onLayoutReady(): Promise<void> {
@@ -23,7 +20,6 @@ export default class PluginSample extends Plugin {
       this.Hpath = "";
       this.notebookId = "";
       this.pageId = "";
-      console.log("切换页面了");
     });
   }
 
@@ -35,15 +31,17 @@ export default class PluginSample extends Plugin {
 
   async handlePaste(e) {
     const { files, protyle } = e.detail;
+    //规定传参数组：[块id,file对象]
     const filesList:any[] = [];
     if (files?.length === 0) return;
     for (let index = 0; index < files.length; index++) {
       let file = files[index];
       const checkNodes = file.type.split("/");
       if (checkNodes[0] !== "image" && this.checkImage(checkNodes[1])) return;
+      //多图上传处理未实现，过滤
       if (files?.length > 1) {
-        showMessage("检测到一次有多个文件上传, 请一次上传文件");
         files.length = 0;
+        showMessage("检测到一次有多个文件上传,如果需要对图片进行压缩,请一次上传文件");
         return;
       }
       //检测图片是否为拖拽插入，是则转换为File对象
@@ -52,7 +50,6 @@ export default class PluginSample extends Plugin {
           type: file.getAsFile().type
      });
     }
-    //console.log(file[Symbol.toStringTag].includes("DataTransferItem"));
       if (this.notebookId + this.Hpath + this.pageId === "") {
         //初始化全局变量: 设置上传路径
         this.pageId = protyle.block.id;
@@ -64,64 +61,14 @@ export default class PluginSample extends Plugin {
       }
       filesList.push([protyle.block.id,file]);
     }
-    //console.log(filesList);
-    //操作图片
     this.processingPictures(filesList);
-
-    // if (files?.length === 0) return;
-    // if (files?.length > 1) {
-    //   new Dialog({
-    //     title: "警告",
-    //     content:
-    //       "检测到一次有多个文件上传,如果继续操作会导致思源笔记出错闪退, 请一次上传文件",
-    //     width: "500px",
-    //     height: "100px",
-    //     // 其他配置...
-    //   });
-    //   files.length = 0;
-    //   return;
-    // }
-    // for (let index = 0; index < files.length; index++) {
-    //   const file = files[index];
-    //   if (this.checkImage(file.type) === "") return;
-    //   if (this.notebookId + this.Hpath + this.pageId === "") {
-    //     //初始化全局变量: 设置上传路径
-    //     this.pageId = protyle.block.id;
-    //     this.notebookId = protyle.notebookId;
-    //     this.Hpath =
-    //       ["assets", (await api.getNotebookConf(this.notebookId)).name].join(
-    //         "/"
-    //       ) + (await api.getHPathByID(this.pageId));
-    //   }
-
-    //   //压缩图片
-    //   const compressedFile = await this.compressImage(file);
-    //   //console.log("修改后的文件名: ",newFileName);
-    //   const renamedFile = new File([compressedFile], `${file.name}`, {
-    //     //type: compressedFile.type
-    //   });
-    //   fileList.push(renamedFile);
-    //   //上传资源图片
-    //   const imagePath = (await api.upload(this.Hpath, [renamedFile])).succMap[
-    //     "image.png"
-    //   ];
-    //   await api.updateBlock(
-    //     "markdown",
-    //     `![${file.name}](${imagePath})`,
-    //     protyle.block.id
-    //   );
-    //   //删除原目录下的图片
-    //   this.remoteAssetsImageFile(await api.readDir("/data/assets"));
-    //}
   }
 
-  //压缩图片并上传
+  //压缩图片
   async processingPictures(files: any[]) {
     const fileList = [];
     for (let index = 0; index < files.length; index++) {
       const file = files[index];
-      //console.log(file);
-      //压缩图片
       const compressedFile = await this.compressImage(file[1]);
       if (compressedFile) {
         fileList.push([file[0],
@@ -129,15 +76,13 @@ export default class PluginSample extends Plugin {
             type: this.compressSuffix,
           })
         ]);
-        //console.log(fileList);
       } else {
         showMessage(`${file[1].name}压缩失败, 请检查图片`);
       }
     }
-    //console.log(fileList);
     this.uploadToSiyuanServer(fileList);
   }
-
+  //上传资源图片
   async uploadToSiyuanServer(files: any[]) {
     const fileList:any[] = [];
     for (let index = 0; index < files.length; index++) {
@@ -149,19 +94,16 @@ export default class PluginSample extends Plugin {
     for (let index = 0; index < files.length; index++) {
       fileList.push([files[index][0],result.succMap[files[index][1].name]]);
     }
-    //console.log(files[0][1]); //id
-    //console.log(fileList[1]); //path
-
-    //await api.updateBlock("markdown", `![${files[0][1].name}](${files[0][1]})`, files[0][0]);
+    //更新块为压缩图片的md语法
     for (let index = 0; index < files.length; index++) {
-      //console.log(await api.updateBlock("markdown", `![${files[index][1].name}](${fileList[index][1]})`, fileList[index][0]));
+      await api.updateBlock("markdown", `![${files[index][1].name}](${fileList[index][1]})`, fileList[index][0]);
     }
-    //console.log(result.succMap);
-    // console.log(await api.readDir("/data/assets"));
+    this.remoteAssetsImageFile(await api.readDir("/data/assets"));
   }
 
+  //删除assets目录下的图片（原资源图片存放路径）
   private async remoteAssetsImageFile(assets: IResReadDir): Promise<void> {
-    for (let index = 0; index < assets.length; index++) {
+    for (let index = 0; index < assets?.length; index++) {
       const element = assets[index];
       if (!element.isDir) {
         await api.removeFile("/data/assets/" + element.name);
@@ -193,11 +135,12 @@ export default class PluginSample extends Plugin {
       );
       return compressedFile;
     } catch (error) {
-      console.error("压缩图片失败:", error);
+      console.log("压缩图片失败:");
       return null;
     }
   }
 
+  //检查是否为合法后缀的图片
   private checkImage(fileType: string): boolean {
     const whiteImageType = ["jpeg", "jpg", "png", "webp"];
     if (!whiteImageType.includes(fileType)) return false;
